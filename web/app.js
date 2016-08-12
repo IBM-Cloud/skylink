e263c2d2695469bc0f9fcd30077abc4c2d67d899
ANDREW M. TRICE <amtrice@us.ibm.com>
2016-08-12 14:43:27 -0400

    added ability to retrain Watson VR classifier with positive reinforcement

--- web/app.js
+++ web/app.js
@@ -5,6 +5,10 @@ var request = require( 'request' );
 var watson = require('watson-developer-cloud');
 var fs = require('fs');
 var Cloudant = require('cloudant');
+var gm = require('gm').subClass({
+      imageMagick: true
+    });
+var archiver = require('archiver');
 
 // Load credentials
 // From Bluemix configuration or local file
@@ -78,6 +82,102 @@ function sortList(a, b) {
     }
     return 0;
 }
+
+
+//handle custom classifier retrain request
+app.post('/:id?/retrain/:posneg?/:classifierId?/:classifierClass?', function (req, res) {
+    var id = req.params.id;
+    var posneg = req.params.posneg;
+    var classifierId = req.params.classifierId;
+    var classifierClass = req.params.classifierClass;
+    var currentTime = new Date().getTime()
+
+    if (posneg == "positive"){
+        //console.log(id, posneg, classifierId, classifierClass)
+        console.log("retraining")
+
+        var rootDir = './temp';
+        var dir = rootDir + "/" + currentTime;
+
+        if (!fs.existsSync(rootDir)){
+            fs.mkdirSync(rootDir);
+        }
+        if (!fs.existsSync(dir)){
+            fs.mkdirSync(dir);
+        }
+
+        var tempFile = dir + "/" + id + ".jpg";
+        var resizedFile = dir + "/" + id + "-resize.jpg";
+        var zipFile = dir + "/" + id + ".zip";
+
+        var cleanup = function() {
+            console.log("cleanup");
+            fs.unlink(tempFile, function (err) { })
+            fs.unlink(resizedFile, function (err) { })
+            fs.unlink(zipFile, function (err) { })
+            fs.rmdir(dir, function (err) { })
+        }
+
+        db.attachment.get(id, "image.jpg", function(err, attachmentBody) {
+            if (err) {
+                res.status(500).send(err.toString());
+                cleanup();
+            } else {
+                //write image to disk
+                fs.writeFileSync(tempFile, attachmentBody);
+
+                //resize image
+                gm(tempFile).define("jpeg:extent=900KB").write(resizedFile,
+                function (err) {
+                    if (err) {
+                        res.status(500).send(err.toString());
+                        cleanup();
+                    } 
+
+                    //create zip containing the image so we can send it to watson
+                    var output = fs.createWriteStream(zipFile);
+                    var archive = archiver('zip');
+                    archive.pipe(output);
+
+                    archive.on('error', function(err) {
+                        res.status(500).send(err.toString());
+                        cleanup();
+                    });
+
+                    archive.on('finish', function(err) {
+
+                        //post positive-reinforcement data to Visual Recognition classifier
+                        var formData = {
+                            api_key:vrCredentials.api_key,
+                            version:"2016-05-20"
+                        };
+                        formData[classifierClass + "_positive_examples"] = fs.createReadStream(zipFile)
+
+                        var url = "https://gateway-a.watsonplatform.net/visual-recognition/api/v3/classifiers/" + classifierId +"?api_key=" + vrCredentials.api_key + "&version=2016-05-20";
+
+                        request.post({url:url, formData: formData}, function optionalCallback(err, httpResponse, body) {
+                            if (err) {
+                                res.status(500).send(err.toString());
+                            } else {
+                                var response = body.toString();
+                                res.status(200).send(response);
+                                console.log(response);
+                            }
+                            cleanup();
+                        });
+
+                    });
+
+                    archive.file(resizedFile, { name: 'image.jpg' });
+                    archive.finalize();
+                });
+            }
+        });
+    } else {
+        res.status(500).send();
+    }
+});
+
 
 
 
